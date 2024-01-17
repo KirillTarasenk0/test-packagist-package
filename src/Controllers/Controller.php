576020b14543ef092864b3256b66e040b8d1962a
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-01-17 16:22:08 +0300

    hello static functions were added to each class

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace Controllers;
 
 class Controller
 {
-
+    public static function helloController(): void
+    {
+        echo "Hello from controller class";
+    }
 }
--- src/Models/Model.php
+++ src/Models/Model.php
@@ -4,5 +4,8 @@ namespace Models;
 
 class Model
 {
-
+    public static function helloModel(): void
+    {
+        echo "Hello from model";
+    }
 }
